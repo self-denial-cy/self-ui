d546dbe51ddc15b176f411d36f2caf6a07e122e2
Jerry <18556134754@163.com>
2024-01-09 19:04:08 +0800

    feat: table 组件开发进度 99%

--- src/App.vue
+++ src/App.vue
@@ -13,12 +13,54 @@
       <self-col :span="6">3</self-col>
       <self-col :span="6">4</self-col>
     </self-row>
-    <self-table shadow></self-table>
+    <div style="height: 16px"></div>
+    <self-table :data="tableData" shadow align="left" hover stripe grid>
+      <self-table-column prop="name" label="姓名" width="25" />
+      <self-table-column prop="age" label="年龄" width="25" />
+      <self-table-column prop="address" label="地址" width="25" ellipsis />
+      <self-table-column prop="remark" label="备注" width="25" />
+    </self-table>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      tableData: [
+        {
+          name: '张三',
+          age: 24,
+          address: '江苏省苏州市吴中区郭巷街道',
+          remark: 'this is zhangsan'
+        },
+        {
+          name: '张三',
+          age: 24,
+          address: '江苏省苏州市吴中区郭巷街道',
+          remark: 'this is zhangsan'
+        },
+        {
+          name: '张三',
+          age: 24,
+          address: '江苏省苏州市吴中区郭巷街道江苏省苏州市吴中区郭巷街道江苏省苏州市吴中区郭巷街道',
+          remark: 'this is zhangsan'
+        },
+        {
+          name: '张三',
+          age: 24,
+          address: '江苏省苏州市吴中区郭巷街道',
+          remark: 'this is zhangsan'
+        },
+        {
+          name: '张三',
+          age: 24,
+          address: '江苏省苏州市吴中区郭巷街道',
+          remark: 'this is zhangsan'
+        }
+      ]
+    };
+  }
 };
 </script>
