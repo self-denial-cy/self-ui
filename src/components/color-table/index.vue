0f701872499eb87f7e6230f5a2cd28488afee0b6
Jerry <18556134754@163.com>
2024-01-25 11:10:07 +0800

    feat: 色彩示例页面开发完成

--- src/components/color-table/index.vue
+++ src/components/color-table/index.vue
@@ -3,8 +3,14 @@
     <self-table-column prop="index" label="序号" width="100" />
     <self-table-column prop="semantic" label="语义" width="100" />
     <self-table-column prop="var" label="变量" width="100" />
-    <self-table-column label="颜色" width="50" />
-    <self-table-column v-for="item in gradient" :key="item" :meta="{ percent: item }" width="50" />
+    <self-table-column label="颜色" width="50" :custom-style="getCustomStyle" />
+    <self-table-column
+      v-for="item in gradient"
+      :key="item"
+      width="50"
+      :meta="{ percent: item }"
+      :custom-style="getGradientCustomStyle"
+    />
     <self-table-column prop="value" label="HEX值" width="100" />
   </self-table>
 </template>
@@ -12,6 +18,25 @@
 <script>
 import SelfTable from 'packages/table';
 import SelfTableColumn from 'packages/table-column';
+
+function lighten(color, amount) {
+  let usePound = false;
+  if (color[0] === '#') {
+    color = color.slice(1);
+    usePound = true;
+  }
+  const val = parseInt(color, 16);
+  let r = (val >> 16) + amount;
+  if (r > 255) r = 255;
+  else if (r < 0) r = 0;
+  let b = ((val >> 8) & 0x00ff) + amount;
+  if (b > 255) b = 255;
+  else if (b < 0) b = 0;
+  let g = (val & 0x0000ff) + amount;
+  if (g > 255) g = 255;
+  else if (g < 0) g = 0;
+  return (usePound ? '#' : '') + (g | (b << 8) | (r << 16)).toString(16);
+}
 
 export default {
   name: 'ColorTable',
@@ -23,6 +48,18 @@ export default {
     data: Array,
     rowStyle: Function,
     gradient: Array
+  },
+  methods: {
+    getCustomStyle(row, rowIndex, column, columnIndex) {
+      return {
+        'background-color': row.value
+      };
+    },
+    getGradientCustomStyle(row, rowIndex, column, columnIndex) {
+      return {
+        'background-color': lighten(row.value, column.meta.percent)
+      };
+    }
   }
 };
 </script>
--- src/views/color/index.vue
+++ src/views/color/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h3>色彩推荐</h3>
+    <color-table :data="tableData" :gradient="gradient" />
   </div>
 </template>
 
@@ -13,8 +14,45 @@ export default {
   },
   data() {
     return {
-      gradient: [10, 20, 30, 40, 50, 60, 70, 80, 90],
-      tableData: []
+      gradient: [10, 30, 50, 70, 90],
+      tableData: [
+        {
+          index: '1',
+          semantic: '主要',
+          var: 'primary',
+          value: '#62a8ea'
+        },
+        {
+          index: '2',
+          semantic: '成功',
+          var: 'success',
+          value: '#46be8a'
+        },
+        {
+          index: '3',
+          semantic: '信息',
+          var: 'info',
+          value: '#57c7d4'
+        },
+        {
+          index: '4',
+          semantic: '警告',
+          var: 'warning',
+          value: '#f2a654'
+        },
+        {
+          index: '5',
+          semantic: '危险',
+          var: 'danger',
+          value: '#f96868'
+        },
+        {
+          index: '6',
+          semantic: '辅助',
+          var: 'gray',
+          value: '#1a1a1a'
+        }
+      ]
     };
   }
 };
