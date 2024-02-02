218a37c0638c6af2607d76f3fe588a194600bc92
Jerry <18556134754@163.com>
2024-02-02 18:33:26 +0800

    feat: 表格示例页面开发

---
+++ src/views/table/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3>表格</h3>
+  </div>
+</template>
+
+<script>
+import SelfTable from 'packages/table';
+import SelfTableColumn from 'packages/table-column';
+
+export default {
+  components: {
+    SelfTable,
+    SelfTableColumn
+  },
+  data() {
+    return {};
+  }
+};
+</script>
