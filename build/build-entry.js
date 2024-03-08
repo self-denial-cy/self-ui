0f3f931ceb2b2cbe41bddbe20455dd147e99a418
Jerry <18556134754@163.com>
2024-03-08 18:18:12 +0800

    feat: 配置整体构建流程

---
+++ build/build-entry.js
@@ -0,0 +1,35 @@
+import Button from '../packages/button';
+import ButtonGroup from '../packages/button-group';
+import Col from '../packages/col';
+import Dropdown from '../packages/dropdown';
+import DropdownItem from '../packages/dropdown-item';
+import Icon from '../packages/icon';
+import Input from '../packages/input';
+import Nav from '../packages/nav';
+import NavItem from '../packages/nav-item';
+import Pagination from '../packages/pagination';
+import Row from '../packages/row';
+import Select from '../packages/select';
+import Table from '../packages/table';
+import TableColumn from '../packages/table-column';
+
+const components = {
+  Button,
+  ButtonGroup,
+  Col,
+  Dropdown,
+  DropdownItem,
+  Icon,
+  Input,
+  Nav,
+  NavItem,
+  Pagination,
+  Row,
+  Select,
+  Table,
+  TableColumn
+};
+
+const install = (Vue, opts = {}) => {
+  if (install.installed) return;
+};
