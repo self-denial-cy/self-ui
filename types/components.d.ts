d98345951437e0a6bc5ccc4032aa8f91316ce925
Jerry <18556134754@163.com>
2024-04-07 23:59:40 +0800

    feat: 编写类型声明文件

--- types/components.d.ts
+++ types/components.d.ts
@@ -2,3 +2,4 @@ export { SelfButton } from './button';
 export { SelfButtonGroup } from './button-group';
 export { SelfCol } from './col';
 export { SelfDropdown } from './dropdown';
+export { SelfDropdownItem } from './dropdown-item';
---
+++ types/dropdown-item.d.ts
@@ -0,0 +1,30 @@
+import Vue from 'vue';
+
+export declare class SelfDropdownItem extends Vue {
+  /**
+   * 当前选项的 value
+   */
+  value?: string | number;
+  /**
+   * 当前选项是否禁用
+   * @default false
+   */
+  disabled?: boolean;
+  /**
+   * list【选项】、title【分组名称】、custom【自定义】、separator【分割线】
+   * @default list
+   */
+  type?: 'list' | 'title' | 'custom' | 'separator';
+  /**
+   * 当前选项图标
+   */
+  icon?: string;
+  /**
+   * 路由导航地址
+   */
+  to?: string;
+  /**
+   * 点击选项时触发
+   */
+  $emit(name: 'on-click', val: string | number): this;
+}
