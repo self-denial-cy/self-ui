7bee43a7ab21f89c668577e2fd164429e64fd0ce
Jerry <18556134754@163.com>
2024-04-08 18:00:57 +0800

    fix: 修复 button 多余类型枚举值

--- types/button.d.ts
+++ types/button.d.ts
@@ -4,7 +4,7 @@ export declare class SelfButton extends Vue {
   /**
    * 按钮语义类型，可选值为 default、dark、primary、success、info、warning、danger、text
    */
-  semantic?: '' | 'default' | 'dark' | 'primary' | 'success' | 'info' | 'warning' | 'danger' | 'text';
+  semantic?: 'default' | 'dark' | 'primary' | 'success' | 'info' | 'warning' | 'danger' | 'text';
   /**
    * 按钮是否开启阴影效果
    * @default false
@@ -13,11 +13,11 @@ export declare class SelfButton extends Vue {
   /**
    * 按钮圆角类型，可选值为 small、large、circle
    */
-  radius?: '' | small | large | circle;
+  radius?: 'small' | 'large' | 'circle';
   /**
    * 按钮大小，可选值为 small、large
    */
-  size?: '' | small | large;
+  size?: 'small' | 'large';
   /**
    * 按钮表单类型，可选值为 reset、button、submit
    * @default button
