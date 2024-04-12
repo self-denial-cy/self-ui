960710eb4825d9bdf22b94fa61f9fa0fe30bb259
Jerry <18556134754@163.com>
2024-04-12 20:03:42 +0800

    feat: 类型声明文件编写

--- types/select.d.ts
+++ types/select.d.ts
@@ -1,5 +1,16 @@
 import Vue from 'vue';
 
+interface IOption {
+  disabled?: boolean;
+  label: string;
+  value: string | number;
+}
+
+interface IGroup {
+  title: string;
+  options: Array<IOption>;
+}
+
 export declare class SelfSelect extends Vue {
   /**
    * 选择框当前选中项，可以使用 sync 修饰符或 v-model 指令
@@ -19,4 +30,19 @@ export declare class SelfSelect extends Vue {
    * @default false
    */
   disabled?: boolean;
+  /**
+   * 选择框可选项数据源
+   * @default []
+   */
+  options?: Array<IOption>;
+  /**
+   * 选择框可选项分组数据源
+   * @default []
+   */
+  groups?: Array<IGroup>;
+  /**
+   * 选择框宽度
+   * @default 150px
+   */
+  width?: string | number;
 }
