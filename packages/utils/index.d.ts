12dbfb31b974db8fef74376652f32a5f07867dd5
Jerry <18556134754@163.com>
2024-04-18 20:08:46 +0800

    feat: 类型声明文件编写

---
+++ packages/utils/index.d.ts
@@ -0,0 +1,5 @@
+export declare function addEventListener(ele: HTMLElement, event: string, handler: (event?: Event) => void, options?: object): void;
+
+export declare function removeEventListener(ele: HTMLElement, event: string, handler: (event?: Event) => void, options?: object): void;
+
+export declare function setClass(): void;
